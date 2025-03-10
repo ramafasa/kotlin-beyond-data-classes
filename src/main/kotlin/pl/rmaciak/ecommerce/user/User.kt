9ef2b2c23ca15b9e20b4ea2b9ba7a85f8590152c
ramac <rafal.maciak@extern.corify.de>
2025-03-10 14:53:21 +0100

    Email extracted to value class.

--- src/main/kotlin/pl/rmaciak/ecommerce/user/User.kt
+++ src/main/kotlin/pl/rmaciak/ecommerce/user/User.kt
@@ -3,7 +3,7 @@ package pl.rmaciak.ecommerce.user
 internal data class User(
     val firstName: String,
     val lastName: String,
-    val email: String,
+    val email: Email,
     val age: Int
 )
 
@@ -15,11 +15,6 @@ internal class UserRegistration {
             throw UserMustBeAnAdultException()
         }
 
-        // provided e-mail address must be valid
-        if (!isValidEmailAddress(user.email)) {
-            throw InvalidEmailAddressException(user.email)
-        }
-
         // user is persisted
         try {
             UserRepository.persist(user)
@@ -28,23 +23,7 @@ internal class UserRegistration {
             return false
         }
     }
-
-    private fun isValidEmailAddress(email: String): Boolean {
-        val emailRegex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
-        return email.matches(emailRegex)
-    }
 }
-
 
 internal class UserMustBeAnAdultException :
     RuntimeException("User must be 18 years or older to register account")
-
-internal class InvalidEmailAddressException(email: String) :
-    RuntimeException("Invalid email address $email")
-
-
-/*
-TODO
-IDEAS:
-jeśli cena pomiędzy 10 .. 100
- */
