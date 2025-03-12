5a66c789b870847fee2f79f98422500e3c81effd
ramac <rafal.maciak@extern.corify.de>
2025-03-12 15:44:33 +0100

    Orders as sealed class

--- src/main/kotlin/pl/rafalmaciak/ecommerce/order/Order.kt
+++ src/main/kotlin/pl/rafalmaciak/ecommerce/order/Order.kt
@@ -1,85 +1,85 @@
 package pl.rafalmaciak.ecommerce.order
 
-import pl.rafalmaciak.ecommerce.order.OrderStatus.CANCELLED
-import pl.rafalmaciak.ecommerce.order.OrderStatus.COMPLETED
-import pl.rafalmaciak.ecommerce.order.OrderStatus.PENDING
-import pl.rafalmaciak.ecommerce.order.OrderStatus.SHIPPED
 import java.util.UUID
 
-internal data class Order(
-    private val orderId: UUID,
-    private val userId: UUID,
-    val status: OrderStatus,
-    private val items: List<OrderItem>,
-    private val totalAmount: Double?,
-    private val shippingAddress: String?,
+
+internal sealed class Order(
+    open val orderId: UUID,
+    open val userId: UUID,
+    open val items: List<OrderItem>
 ) {
 
-    fun cancelOrder(): Order =
-        check(status in listOf(PENDING, COMPLETED)) {
-            "Order in status $status cannot be cancelled"
-        }.let {
-            this.copy(status = CANCELLED)
-        }
-
-    fun addOrderItem(orderItem: OrderItem): Order {
-        check(status == PENDING) {
-            "Order in status $status cannot be modified"
-        }
-
-        return this.copy(
-            items = this.items + orderItem,
-            totalAmount = totalAmount?.plus(orderItem.price * orderItem.quantity)
-                ?: (orderItem.price * orderItem.quantity),
-        )
+    interface CancellableOrder {
+        fun cancelOrder(): CancelledOrder
     }
 
-    fun getOrderTotalAmount(): Double =
-        totalAmount ?: 0.0
+    internal data class PendingOrder(
+        override val orderId: UUID,
+        override val userId: UUID,
+        override val items: List<OrderItem>,
+    ) : Order(orderId, userId, items), CancellableOrder {
 
-    fun completeOrder(): Order {
-        check(status == PENDING) { "Order in status $status cannot be completed" }
-        check(items.isNotEmpty()) { "Order items must not be empty" }
-        return this.copy(status = COMPLETED)
-    }
-
-    fun shipOrder(shippingAddress: String): Order =
-        check(status == COMPLETED) {
-            "Order in status $status cannot be shipped"
-        }.let {
+        fun addOrderItem(orderItem: OrderItem): PendingOrder =
             this.copy(
-                status = SHIPPED,
+                items = this.items + orderItem,
+            )
+
+        fun completeOrder(): CompletedOrder {
+            check(items.isNotEmpty()) { "Order items must not be empty" }
+            return CompletedOrder(orderId, userId, items, items.sumOf { it.price * it.quantity })
+        }
+
+        override fun cancelOrder(): CancelledOrder =
+            CancelledOrder(orderId, userId, items)
+
+        companion object {
+            fun createPendingOrder(
+                userId: UUID,
+            ) {
+                PendingOrder(
+                    orderId = UUID.randomUUID(),
+                    userId = userId,
+                    items = emptyList(),
+                )
+            }
+        }
+    }
+
+    internal data class CompletedOrder(
+        override val orderId: UUID,
+        override val userId: UUID,
+        override val items: List<OrderItem>,
+        val totalAmount: Double,
+    ) : Order(orderId, userId, items), CancellableOrder {
+
+        fun getOrderTotalAmount(): Double = totalAmount
+
+        override fun cancelOrder(): CancelledOrder =
+            CancelledOrder(orderId, userId, items)
+
+        fun shipOrder(shippingAddress: String): ShippedOrder =
+            ShippedOrder(
+                orderId = orderId,
+                userId = userId,
+                items = items,
+                totalAmount = totalAmount,
                 shippingAddress = shippingAddress,
             )
-        }
-
-    fun getShippingAddress(): String {
-        check(status == SHIPPED) { "Order in status $status does not have a shipping address" }
-        check(shippingAddress != null) { "Shipping address is not set" }
-        return requireNotNull(shippingAddress)
     }
 
-    companion object {
-        fun createPendingOrder(
-            userId: UUID,
-        ) {
-            Order(
-                orderId = UUID.randomUUID(),
-                userId = userId,
-                status = PENDING,
-                items = emptyList(),
-                totalAmount = null,
-                shippingAddress = null,
-            )
-        }
-    }
-}
+    internal data class ShippedOrder(
+        override val orderId: UUID,
+        override val userId: UUID,
+        override val items: List<OrderItem>,
+        val totalAmount: Double,
+        val shippingAddress: String,
+    ) : Order(orderId, userId, items)
 
-internal enum class OrderStatus {
-    PENDING,
-    COMPLETED,
-    SHIPPED,
-    CANCELLED,
+    internal data class CancelledOrder(
+        override val orderId: UUID,
+        override val userId: UUID,
+        override val items: List<OrderItem>,
+    ) : Order(orderId, userId, items)
 }
 
 internal data class OrderItem(
--- src/test/kotlin/pl/rafalmaciak/ecommerce/order/OrderTests.kt
+++ src/test/kotlin/pl/rafalmaciak/ecommerce/order/OrderTests.kt
@@ -2,149 +2,93 @@ package pl.rafalmaciak.ecommerce.order
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.matchers.doubles.shouldBeExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import pl.rafalmaciak.ecommerce.order.OrderStatus.CANCELLED
-import pl.rafalmaciak.ecommerce.order.OrderStatus.COMPLETED
-import pl.rafalmaciak.ecommerce.order.OrderStatus.PENDING
-import pl.rafalmaciak.ecommerce.order.OrderStatus.SHIPPED
+import io.kotest.matchers.types.shouldBeInstanceOf
+import pl.rafalmaciak.ecommerce.order.Order.CancelledOrder
 import java.util.UUID
 
 class OrderTests : ShouldSpec({
 
-    fun createOrder(
-        status: OrderStatus,
-        items: List<OrderItem> = emptyList(),
-        totalAmount: Double? = null,
-        shippingAddress: String? = null
-    ): Order {
-        return Order(
-            orderId = UUID.randomUUID(),
-            userId = UUID.randomUUID(),
-            status = status,
-            items = items,
-            totalAmount = totalAmount,
-            shippingAddress = shippingAddress
-        )
-    }
-
     context("cancelling order") {
-        should("cancel an order with PENDING status") {
-            val order = createOrder(PENDING)
+        should("cancel a pending order") {
+            val order = Order.PendingOrder(
+                orderId = UUID.randomUUID(),
+                userId = UUID.randomUUID(),
+                items = emptyList(),
+            )
+
             val cancelledOrder = order.cancelOrder()
-            cancelledOrder.status shouldBe CANCELLED
+
+            cancelledOrder.shouldBeInstanceOf<CancelledOrder>()
         }
 
-        should("cancel an order with COMPLETED status") {
-            val order = createOrder(COMPLETED)
+        should("cancel a completed order") {
+            val order = Order.CompletedOrder(
+                orderId = UUID.randomUUID(),
+                userId = UUID.randomUUID(),
+                items = emptyList(),
+                totalAmount = 0.0
+            )
+
             val cancelledOrder = order.cancelOrder()
-            cancelledOrder.status shouldBe CANCELLED
-        }
 
-        should("fail to cancel an order with SHIPPED status") {
-            val order = createOrder(SHIPPED)
-            val exception = shouldThrow<IllegalStateException> { order.cancelOrder() }
-            exception.message shouldContain "Order in status SHIPPED cannot be cancelled"
-        }
-
-        should("fail to cancel an order with CANCELLED status") {
-            val order = createOrder(CANCELLED)
-            val exception = shouldThrow<IllegalStateException> { order.cancelOrder() }
-            exception.message shouldContain "Order in status CANCELLED cannot be cancelled"
-        }
-    }
-
-    context("adding item to order") {
-        val orderItem = OrderItem(
-            productId = UUID.randomUUID(),
-            quantity = 2,
-            price = 50.0
-        )
-
-        should("add an order item to an order with PENDING status") {
-            val order = createOrder(PENDING, items = emptyList(), totalAmount = 0.0)
-            val updatedOrder = order.addOrderItem(orderItem)
-
-            updatedOrder.getOrderTotalAmount().shouldBeExactly(50.0 * 2)
-        }
-
-        listOf(COMPLETED, SHIPPED, CANCELLED).forEach { status ->
-            should("fail to add an order item to an order with $status status") {
-                val order = createOrder(status)
-                val exception = shouldThrow<IllegalStateException> { order.addOrderItem(orderItem) }
-                exception.message shouldContain "Order in status $status cannot be modified"
-            }
-        }
-    }
-
-    context("retrieving total amount of order") {
-        should("return the provided totalAmount when not null") {
-            val order = createOrder(COMPLETED, totalAmount = 100.0)
-            order.getOrderTotalAmount() shouldBe 100.0
-        }
-
-        should("return 0.0 when totalAmount is null") {
-            val order = createOrder(COMPLETED, totalAmount = null)
-            order.getOrderTotalAmount() shouldBe 0.0
+            cancelledOrder.shouldBeInstanceOf<CancelledOrder>()
         }
     }
 
     context("completing order") {
-        should("complete an order with PENDING status and non-empty items") {
+        should("complete a pending order with non-empty items") {
             val orderItem = OrderItem(
                 productId = UUID.randomUUID(),
                 quantity = 1,
                 price = 100.0
             )
 
-            val order = createOrder(PENDING, items = listOf(orderItem), totalAmount = 100.0)
-            val completedOrder = order.completeOrder()
+            val order = Order.PendingOrder(
+                orderId = UUID.randomUUID(),
+                userId = UUID.randomUUID(),
+                items = emptyList(),
+            )
 
-            completedOrder.status shouldBe COMPLETED
-        }
+            val updatedOrder = order.addOrderItem(orderItem)
+            val completedOrder = updatedOrder.completeOrder()
 
-        listOf(COMPLETED, SHIPPED, CANCELLED).forEach { status ->
-            should("fail to complete an order with $status status") {
-                val order = createOrder(
-                    status,
-                    items = listOf(OrderItem(UUID.randomUUID(), 1, 50.0)),
-                    totalAmount = 50.0
-                )
-                val exception = shouldThrow<IllegalStateException> { order.completeOrder() }
-                exception.message shouldContain "Order in status $status cannot be completed"
-            }
+            completedOrder.getOrderTotalAmount() shouldBe 100
+            completedOrder.shouldBeInstanceOf<Order.CompletedOrder>()
         }
 
         should("fail to complete an order with empty items") {
-            val order = createOrder(PENDING, items = emptyList(), totalAmount = 0.0)
+            val order = Order.PendingOrder(
+                orderId = UUID.randomUUID(),
+                userId = UUID.randomUUID(),
+                items = emptyList(),
+            )
+
             val exception = shouldThrow<IllegalStateException> { order.completeOrder() }
+
             exception.message shouldContain "Order items must not be empty"
         }
     }
 
     context("shipping order and getting shipping address") {
         should("ship an order and set the shipping address") {
-            val order = createOrder(COMPLETED)
+            val orderItem = OrderItem(
+                productId = UUID.randomUUID(),
+                quantity = 1,
+                price = 100.0
+            )
+
+            val order = Order.CompletedOrder(
+                orderId = UUID.randomUUID(),
+                userId = UUID.randomUUID(),
+                items = listOf(orderItem),
+                totalAmount = 100.0
+            )
+
             val shippedOrder = order.shipOrder("123 Main St")
-            shippedOrder.getShippingAddress() shouldBe "123 Main St"
-            shippedOrder.status shouldBe SHIPPED
-        }
-
-        should("fail to get shipping address when not set") {
-            val nonShippedOrder = createOrder(SHIPPED)
-            val exception =
-                shouldThrow<IllegalStateException> { nonShippedOrder.getShippingAddress() }
-            exception.message shouldContain "Shipping address is not set"
-        }
-
-        listOf(PENDING, SHIPPED, CANCELLED).forEach { status ->
-            should("fail to ship an order that is in status $status") {
-                val order = createOrder(status)
-                val exception =
-                    shouldThrow<IllegalStateException> { order.shipOrder("123 Main St") }
-                exception.message shouldContain "Order in status $status cannot be shipped"
-            }
+            shippedOrder.shippingAddress shouldBe "123 Main St"
+            shippedOrder.shouldBeInstanceOf<Order.ShippedOrder>()
         }
     }
 })
