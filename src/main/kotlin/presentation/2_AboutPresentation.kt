1cec0005467c9a22381f3758b83b2a2c37e39d0b
ramac <rafal.maciak@extern.corify.de>
2025-03-13 15:52:47 +0100

    Presentation

---
+++ src/main/kotlin/presentation/2_AboutPresentation.kt
@@ -0,0 +1,48 @@
+package presentation
+
+const val aboutPresentation = """
+    
+    What this presentation is about?
+    ================================
+    
+    * Improving domain model code to become:
+        - More readable (because we mainly read the code)
+        - Less error prone
+        - Easier to test
+        - Resilient to requirements changes
+        - More fun to write :)
+    
+    * Live refactoring to transform the anemic code into more readable, idiomatic and efficient 
+      Kotlin code using its features:
+        - Value class
+        - Range operator
+        - Result class
+        - Sealed class
+        - Companion object
+        - Default arguments
+        - Extension functions
+        - Operation overloading
+        - Bonus: DSL for tests
+   
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
