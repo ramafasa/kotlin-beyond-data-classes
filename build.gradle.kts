ea7188a66100ac25abc5ceff9705ea82cdd9acae
ramac <rafal.maciak@extern.corify.de>
2025-03-14 15:12:33 +0100

    Branch aligning

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     dependencies {
+        implementation("org.apache.poi:poi:5.2.3")
+        implementation("org.apache.poi:poi-ooxml:5.2.3")
         testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
         testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     }
