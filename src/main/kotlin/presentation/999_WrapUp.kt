9e311ba70e46e7e8a9182b7fa64a83827f081bf1
ramac <rafal.maciak@extern.corify.de>
2025-03-16 09:56:05 +0100

    Branch aligning

--- src/main/kotlin/presentation/999_WrapUp.kt
+++ src/main/kotlin/presentation/999_WrapUp.kt
@@ -1,11 +1,16 @@
 package presentation
 
 const val theEnd = """
-    
+   
         To wrap-up:
         - We have seen how to refactor anemic, not expressive code into a more expressive, 
-          domain-driven model using Kotlin features.
-        - Remember to consciously choose the code that you want to invest and implement rich model.
+          domain-driven model using Kotlin features
+          
+          
+          Questions?
+          
+          
+          Thank you!
 
 
 
