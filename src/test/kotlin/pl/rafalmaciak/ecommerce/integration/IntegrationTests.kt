b510aaa7cddb80377d9b09cb1849593234803967
ramac <rafal.maciak@extern.corify.de>
2025-05-13 21:09:01 +0200

    Fixed example

---
+++ src/test/kotlin/pl/rafalmaciak/ecommerce/integration/IntegrationTests.kt
@@ -0,0 +1,42 @@
+package pl.rafalmaciak.ecommerce.integration
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import pl.rafalmaciak.ecommerce.order.Order
+import pl.rafalmaciak.ecommerce.order.Order.CompletedOrder
+import pl.rafalmaciak.ecommerce.order.OrderItem
+import pl.rafalmaciak.ecommerce.user.UserDto
+import pl.rafalmaciak.ecommerce.user.UserRegistration
+import pl.rafalmaciak.ecommerce.user.UserRegistrationResult.UserRegistered
+import java.util.UUID
+
+
+class IntegrationTests : ShouldSpec({
+
+    should("create user and order") {
+        // given user is registered
+        val userDto = UserDto("John", "Doe", "john.doe@example.com", 30)
+        val user = when (val user = UserRegistration.registerUser(userDto)) {
+            is UserRegistered -> user.user
+            else -> throw AssertionError("Expected UserRegistered but got $user")
+        }
+
+        // and order exists
+        var order = Order.PendingOrder(
+            orderId = UUID.randomUUID(),
+            userId = user.id.raw,
+            items = emptyList()
+        )
+
+        order = order.addOrderItem(OrderItem(UUID.randomUUID(), 2, 50.0))
+        order = order.addOrderItem(OrderItem(UUID.randomUUID(), 1, 100.0))
+
+        // when completing the order
+        val completedOrder = order.completeOrder()
+
+        // then the order is completed
+        completedOrder.getOrderTotalAmount() shouldBe 200
+        completedOrder.shouldBeInstanceOf<CompletedOrder>()
+    }
+})
